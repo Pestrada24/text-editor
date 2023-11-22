5cea1d7c10e9f5b1e87820f6b6d8d3343823de61
Pestrada24 <pestrada74@gmail.com>
2023-11-21 22:38:51 -0500

    added loaders and plugins

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,48 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = {
+  // other webpack configurations...
+
+  plugins: [
+    // other plugins...
+
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+};
+
+
+module.exports = {
+  // other webpack configurations...
+
+  module: {
+    rules: [
+      // CSS loader
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Babel loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
+
 
 module.exports = () => {
   return {
